dad08821b0dbfb50d978f3c982013d5ba25a15db
jose julian Lopez Huacuja <jose.lopez4636@alumnos.udg.mx>
2025-04-10 13:56:39 -0600

    Initial Commit

---
+++ Momo_Tests-01/Momo_Tests_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Momo_Tests_01App.swift
+//  Momo_Tests-01
+//
+//  Created by Jose julian Lopez on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Momo_Tests_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
