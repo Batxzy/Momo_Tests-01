984ac69351a85931ba58bb11b8b0bc9f8484d9ce
jose julian Lopez Huacuja <jose.lopez4636@alumnos.udg.mx>
2025-04-11 01:40:36 -0600

    da changes to the scroll

--- Momo_Tests-01/GameManagment/levelManager.swift
+++ Momo_Tests-01/GameManagment/levelManager.swift
@@ -15,7 +15,6 @@ class LevelManager {
     var currentLevelIndex: Int
     var isTransitioning: Bool = false
     var transitionType: LevelTransition?
-    var transitionDirection: TransitionDirection = .next
     var itemSpacing: CGFloat = 40 // Spacing between carousel items
     
     // For update tracking
@@ -38,46 +37,7 @@ class LevelManager {
         currentChapterIndex + 1 < chapters.count
     }
     
-    enum TransitionDirection {
-        case next, previous
-    }
     
-    // Get the next logical level index for transitions
-    func getNextLevelIndex() -> Int {
-        let totalLevels = currentChapter.levels.count
-        if totalLevels < 2 { return currentLevelIndex } // No change if fewer than 2 levels
-        
-        let nextLevelIndex: Int
-        if transitionDirection == .next {
-            // Going forward
-            nextLevelIndex = (currentLevelIndex < totalLevels - 1) ? currentLevelIndex + 1 : 0
-        } else {
-            // Going backward
-            nextLevelIndex = (currentLevelIndex > 0) ? currentLevelIndex - 1 : totalLevels - 1
-        }
-        
-        // Toggle direction for next time
-        transitionDirection = transitionDirection == .next ? .previous : .next
-        
-        return nextLevelIndex
-    }
-    
-    // For debugging purposes - trigger transitions between views
-    func debugTriggerTransition(_ type: LevelTransition) {
-        print("DEBUG: Manually triggering \(type) transition between views")
-        
-        // Just use the fade transition directly
-        if type == .fade {
-            // Set transition type and start transitioning
-            transitionType = type
-            isTransitioning = true
-            
-            // Change level immediately
-            currentLevelIndex = getNextLevelIndex()
-            updateCounter += 1
-        }
-        // Camera pan transitions are now handled via ScrollView in ContentView
-    }
     
     // Regular level completion transitions
     func completeCurrentLevel() {
@@ -90,10 +50,6 @@ class LevelManager {
         
         chapters[currentChapterIndex].levels[currentLevelIndex].isCompleted = true
         
-        // Retain current transition style before changing state
-        let transitionStyle = currentLevel.transition
-        transitionType = transitionStyle
-        transitionDirection = .next // Always move forward on completion
         isTransitioning = true
                 
         // Update level/chapter immediately - transition is handled by SwiftUI 
@@ -115,14 +71,6 @@ class LevelManager {
         lastActionLog = "Game completed!"
     }
     
-    func checkWinCondition(scrollPosition: ScrollPosition? = nil, minigameCompleted: String? = nil) -> Bool {
-        switch currentLevel.winCondition {
-        case .completeMinigame(let id):
-            return minigameCompleted == id
-        case .custom(let condition):
-            return condition()
-        }
-    }
     
     init(chapters: [Chapter]) {
         self.chapters = chapters
--- Momo_Tests-01/Momo_Tests_01App.swift
+++ Momo_Tests-01/Momo_Tests_01App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Momo_Tests_01App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Test_Transition()
         }
     }
 }
